2e25bc3d20cb16901496252c469773dc3e2e4724
Eduardo Diez <edudm95@gmail.com>
2022-11-10 12:00:03 +0100

    Incluido dado con varias caras

--- src/APIdado.js
+++ src/APIdado.js
@@ -1,16 +1,22 @@
-const express = require('express')
+const express = require('express');
 
-function rollDice(){
+function rollDice(caras = 6){
     const min = 1;
-    const max = 6;
+    const max = caras;
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 const app = express();
 
 app.get('/rollDice', (req, res) => {
-    res.json({tirada : rollDice()})
+    res.json({tirada : rollDice()});
 })
 
-app.listen(3000, console.log("Estoy ecuchando en el puerto 3000"))
+app.get('/rollDice/:caras', (req, res) => {
+    const caras = req.params.caras
+    res.json({tirada : rollDice(caras)});
+})
+
+app.listen(3000, console.log("Estoy ecuchando en el puerto 3000"));
+
 
